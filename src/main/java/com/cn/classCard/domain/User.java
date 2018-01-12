164547eb66802849c7e61f38fd31a3f4185eee0f
liuhx <liuhx>
2018-01-12 18:34:26 +0800

    add the front page to show。

--- src/main/java/com/cn/classCard/domain/User.java
+++ src/main/java/com/cn/classCard/domain/User.java
@@ -7,38 +7,68 @@ public class User {
     private String id;
     private String userName;
     private Integer age;
+    private String sex;
     private String customerId;
+    private String role;
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getUserName() {
         return userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
     public Integer getAge() {
         return age;
     }
+
     public void setAge(Integer age) {
         this.age = age;
     }
+
     public String getCustomerId() {
         return customerId;
     }
+
     public void setCustomerId(String customerId) {
         this.customerId = customerId;
     }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
+                "id='" + id + '\'' +
                 ", userName='" + userName + '\'' +
                 ", age=" + age +
-                ", customerId=" + customerId +
+                ", sex='" + sex + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", role='" + role + '\'' +
                 '}';
     }
+
 }
